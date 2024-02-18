3e84e8cff59830ae3e1ce60678ce6c1ce51973a2
impactgalaxy <traderglow@gmail.com>
2024-02-19 00:56:18 +0600

    final touch

--- script/app.js
+++ script/app.js
@@ -14,7 +14,7 @@ function myApp() {
             const getInnerText = seat.innerText;
             setBackgroundColorById(getInnerText);
 
-            if (seat.classList.contains("bg-orange-600")) {
+            if (seat.classList.contains("bg-green-600")) {
 
                 if (arr.length > 3) {
                     alert("You can't by over 4 seats");
@@ -22,6 +22,7 @@ function myApp() {
                     count = count;
                     removeBackgroundColorById(getInnerText);
                     addValueById("avail-seats", totalSeats);
+                    addValueById("show", totalSeats);
                     addValueById("count", count);
                 }
                 else {
@@ -31,7 +32,7 @@ function myApp() {
                     const td2 = document.createElement("td");
                     const td3 = document.createElement("td");
                     td1.innerText = getInnerText;
-                    td2.innerText = "Economy Class";
+                    td2.innerText = "Economy";
                     td3.innerText = "550";
                     td3.setAttribute("class", "price");
                     tr.append(td1, td2, td3);
@@ -40,6 +41,7 @@ function myApp() {
                     addValueById("total-price", totalPrice += 550);
                     addValueById("grand-total", totalPrice);
                     addValueById("avail-seats", totalSeats);
+                    addValueById("show", totalSeats);
                     count += 1
                     addValueById("count", count);
                 }
@@ -48,6 +50,7 @@ function myApp() {
                 totalSeats += 1;
                 arr.pop();
                 addValueById("avail-seats", totalSeats);
+                addValueById("show", totalSeats);
                 tbody.removeChild(tbody.lastElementChild);
                 addValueById("total-price", totalPrice -= 550);
                 addValueById("grand-total", totalPrice);
@@ -76,16 +79,12 @@ function couponApply() {
     const totalPrice = document.getElementById("total-price").innerText;
     const newTotal = parseFloat(totalPrice);
 
-
-
-
-
-    if (couponCode.value === "NEW15") {
+    if (couponCode.value === "NEW15" && newTotal !== 0) {
         const discounted = newTotal * 15 / 100;
         discountShow.innerText = `Congratulations! You have got ${discounted} taka discount`;
         addValueById("grand-total", newTotal - discounted);
 
-    } else if (couponCode.value === "Couple20") {
+    } else if (couponCode.value === "Couple20" && newTotal !== 0) {
         const discounted = newTotal * 20 / 100;
         discountShow.innerText = `Congratulations! You have got ${discounted} taka discount`;
         addValueById("grand-total", newTotal - discounted);
@@ -97,6 +96,34 @@ function couponApply() {
 
     couponCode.value = "";
     couponBtn.setAttribute("disabled", true);
+
+}
+const form = document.getElementById("form");
+const message = document.getElementById("message");
+
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+    const userNumber = document.getElementById("user-number");
+
+    const price = document.getElementById("total-price").innerText;
+    if (userNumber.value === "" || parseInt(price) === 0) {
+        alert("Please input you number\nOr by at least 1 seat");
+    }
+    else {
+        message.classList.add("flex");
+        message.classList.remove("hidden");
+        hideElement("main");
+        hideElement("footer");
+        hideElement("header");
+    }
+})
+function display() {
+    hideElement("message");
+    message.classList.remove("flex");
+    displayElement("header");
+    displayElement("main")
+    displayElement("footer");
+    location.reload();
 
 }
 
--- script/utility.js
+++ script/utility.js
@@ -1,12 +1,20 @@
 function setBackgroundColorById(elementId) {
     const getId = document.getElementById(elementId);
-    getId.classList.toggle("bg-orange-600");
+    getId.classList.toggle("bg-green-600");
 }
 function removeBackgroundColorById(elementId) {
     const getId = document.getElementById(elementId);
-    getId.classList.remove("bg-orange-600");
+    getId.classList.remove("bg-green-600");
 }
 function addValueById(elementId, value) {
     const getId = document.getElementById(elementId);
     getId.innerText = value;
+}
+function hideElement(elementId) {
+    const getId = document.getElementById(elementId);
+    getId.classList.add("hidden");
+}
+function displayElement(elementId) {
+    const getId = document.getElementById(elementId);
+    getId.classList.add("block");
 }
